9b34ac157da9da617789d460b5789e31031fe3cc
Karishma Patni <i22290@corp.lci.com>
2021-02-16 21:22:59 -0800

    Updates

---
+++ precache-manifest.ac73c9b02e19d4267def4c0df639cdd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f101e245d483401c630216fce196368d",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "fb27e2547e68f47f0438",
+    "url": "/Movie-App/static/css/main.462c1f05.chunk.css"
+  },
+  {
+    "revision": "293a6987ad8ae89d09e9",
+    "url": "/Movie-App/static/js/2.6529f89a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Movie-App/static/js/2.6529f89a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb27e2547e68f47f0438",
+    "url": "/Movie-App/static/js/main.2b964adf.chunk.js"
+  },
+  {
+    "revision": "5fc1abd888e939008dca",
+    "url": "/Movie-App/static/js/runtime-main.6bae7b49.js"
+  }
+]);
